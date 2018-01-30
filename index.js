51018a3fca50943175df8e310b2a81e9227fcd22
CADBOT <charliec364@gmail.com>
2018-01-30 11:09:49 -0800

    finish donut routes

--- index.js
+++ index.js
@@ -24,11 +24,30 @@ function getDonutById(id) {
   return db.donuts.find(donut => donut.id == id)
 }
 
+// route
+let express = require('express')
+let bodyParser = require('body-parser')
+let app = express()
 
-/* *tests*
-createDonut('sprinkles', 5000, 'tasty', 5)
-let allDonuts = getAllDonuts()
-console.log('all donuts', allDonuts)
-let donut = getDonutById(allDonuts[0].id)
-console.log(donut)
-*/
+app.use(bodyParser.json())
+
+// GET routes
+app.get('/donuts', (req, res) => {
+  let donuts = getAllDonuts()
+  res.json({data: donuts})
+})
+
+app.get('/donuts/:id', (req, res) => {
+  let id = req.params.id
+  let donut = getDonutById(id)
+  res.json({data: donut})
+})
+
+app.post('/donuts', (req, res) => {
+  let {type, price, brand, rating} = req.body
+  let donut = createDonut(type, price, brand, rating)
+  res.json({data: donut})
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
